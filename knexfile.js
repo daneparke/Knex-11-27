1ec5920b0839eebcfee33cf2c26cc9747585c74a
daneparke <dapa8522@colorado.edu>
2018-11-27 11:57:50 -0700

    Configured Knex for development/production

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      filename: 'postgresql://localhost/DATABASE_NAME_THAT_MAKES_SENSE_FOR_G102_DATABASE'
+    },
+    production: {
+      client: 'pg',
+      connection: process.env.DATABASE_URL
+    }
+  }
+};
