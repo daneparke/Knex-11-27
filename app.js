99bf9c269b7b71d1e26f4a57c43c06b3f1f5a7e6
daneparke <dapa8522@colorado.edu>
2018-11-28 09:29:38 -0700

    added get by id, and it works

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3003
 const queries = require('./queries')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     queries.getAll().then(response => res.send(response))
 })
+
+app.get('/:id', (req, res) => {
+    const { id } = req.params
+    queries.getById(id).then(response => res.send(response))
+})
+
 
 
 const listener = () => console.log(`Listening on port ${port}`)
--- queries.js
+++ queries.js
@@ -3,5 +3,8 @@ const db = require('./database-connection')
 module.exports = {
     getAll() {
         return db.select().from('students')
+    },
+    getById(id) {
+        return db('students').where({ id: id }).first()
     }
 };
