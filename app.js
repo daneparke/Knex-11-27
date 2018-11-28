bb25faa224619b2c95f28275ce9058aae9be04e7
daneparke <dapa8522@colorado.edu>
2018-11-28 10:09:34 -0700

    added post route, working

--- app.js
+++ app.js
@@ -9,14 +9,17 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    queries.getAll().then(response => res.send(response))
+    queries.getAll().then(students => res.send(students))
 })
 
 app.get('/:id', (req, res) => {
     const { id } = req.params
-    queries.getById(id).then(response => res.send(response))
+    queries.getById(id).then(student => res.send(student))
 })
 
+app.post('/', (req, res) => {
+    queries.createStudent(req.body).then(res.status(201))
+})
 
 
 const listener = () => console.log(`Listening on port ${port}`)
--- queries.js
+++ queries.js
@@ -2,9 +2,15 @@ const db = require('./database-connection')
 
 module.exports = {
     getAll() {
-        return db.select().from('students')
+        return db('students')
     },
+    // getAll() {
+    //     return db.select().from('students')
+    // },
     getById(id) {
         return db('students').where({ id: id }).first()
+    },
+    createStudent(newStudent) {
+        return db('students').insert(newStudent)
     }
 };
